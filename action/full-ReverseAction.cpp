e62e3953069064d7a5ae452f41009359e9f0405e
FuduDufu <145094426+FuduDufu@users.noreply.github.com>
2024-09-06 15:15:00 +0800

    Update full-ReverseAction.cpp

--- action/full-ReverseAction.cpp
+++ action/full-ReverseAction.cpp
@@ -36,7 +36,7 @@ void reverseTypeII(int X, int Y, int size, int s)
     int chosenRowNum = h / 2;
     bool first = (h + 1) % 2;
 
-    if(oby >= n){
+    if((Y + size - 1) >= n){
         if(!first) chosenRowNum++;
         first = h % 2;
     }
@@ -56,6 +56,7 @@ void reverseTypeII(int X, int Y, int size, int s)
                 cout << board[index_y][index_x] << ' ';
             }
             cout << '\n';
+            
         }
 
         cout << "CHOSEN\n";
@@ -150,47 +151,12 @@ void reverseTypeII(int X, int Y, int size, int s)
         }
 
     }
+// 
 
-    if (s==2) { 
+
+    if (s==2 ) {
         int count = 0 ; 
-        for(int index_y = oty; index_y <=oby; index_y+=2){
-            for(int index_x = w; index_x <=obx; index_x++){                        
-                rem[count].push_back(board[index_y][index_x]);
-            }  
-            count ++ ; 
-            cout<< endl; 
-        }
-        count = 0 ; 
-        for(int index_y = oty; index_y <=oby; index_y+=2){
-            for(int index_x = 0;  index_x < w; index_x++){ 
-                    chosen[count].push_back(board[index_y][index_x]);    
-                }
-                count ++ ; 
-            }
-        int cnt = 0;
-        for ( int index_x=0 ; index_x<=obx; index_x++){
-            if ( index_x < otx){
-                cnt= 0 ; 
-                for ( int index_y=oty ; index_y<=oby; index_y+=2 ){
-                    board[index_y][index_x]=rem[cnt][index_x] ; 
-                    cnt ++ ;   
-                }
-            }
-            else {
-                cnt = 0 ; 
-                for ( int index_y=oty ; index_y<=oby; index_y+=2 ){
-                    board[index_y][index_x]= chosen[cnt][index_x-otx] ; 
-                    cnt ++ ; 
-                }
-            }
-        }
-    }
-
-
-
-    if (s==3 ) {
-        int count = 0 ; 
-        for(int index_y = oty; index_y <= oby; index_y+=2){
+        for(int index_y = ((oby-oty+1)%2==0 ? oty: oty+1); index_y <= oby; index_y+=2){
             for(int index_x = otx; index_x < n-w; index_x++){                        
                 rem[count].push_back(board[index_y][index_x]);
                 cout << board[index_y][index_x] << " " ; 
@@ -200,7 +166,7 @@ void reverseTypeII(int X, int Y, int size, int s)
         }
         cout << endl ; 
         count = 0 ; 
-        for(int index_y = oty; index_y <=oby; index_y+=2){
+        for(int index_y = ((oby-oty+1)%2==0 ? oty: oty+1); index_y <=oby; index_y+=2){
              for(int index_x = n-w;  index_x < n; index_x++){ 
                 chosen[count].push_back(board[index_y][index_x]);     
                 cout << board[index_y][index_x]<< " " ;           
@@ -209,13 +175,11 @@ void reverseTypeII(int X, int Y, int size, int s)
             count ++ ; 
    
         }
-
-
         int cnt = 0;
         for ( int index_x=otx ; index_x<n ; index_x++){
             if ( index_x <=obx ){
                 cnt= 0 ; 
-                for ( int index_y=oty ; index_y<=oby; index_y+=2 ){
+                for ( int index_y=((oby-oty+1)%2==0 ? oty: oty+1); index_y<=oby; index_y+=2 ){
                     board[index_y][index_x]=chosen[cnt][index_x-otx] ; 
                     cnt ++ ; 
                     
@@ -224,7 +188,7 @@ void reverseTypeII(int X, int Y, int size, int s)
             }
             else {
                 cnt = 0 ; 
-                for ( int index_y=oty ; index_y<=oby; index_y+=2 ){
+                for ( int index_y=((oby-oty+1)%2==0 ? oty: oty+1) ; index_y<=oby; index_y+=2 ){
                     board[index_y][index_x]= rem[cnt][index_x -(obx+1)] ; 
                   
                     cnt ++ ; 
@@ -233,10 +197,40 @@ void reverseTypeII(int X, int Y, int size, int s)
         }
            
     }
-    
-    
-
-
+    if (s==3) { 
+        int count = 0 ; 
+        for(int index_y = ((oby-oty+1)%2==0 ? oty: oty+1); index_y <=oby; index_y+=2){
+            for(int index_x = w; index_x <=obx; index_x++){                        
+                rem[count].push_back(board[index_y][index_x]);
+            }  
+            count ++ ; 
+            cout<< endl; 
+        }
+        count = 0 ; 
+        for(int index_y = ((oby-oty+1)%2==0 ? oty: oty+1); index_y <=oby; index_y+=2){
+            for(int index_x = 0;  index_x < w; index_x++){ 
+                    chosen[count].push_back(board[index_y][index_x]);    
+                }
+                count ++ ; 
+            }
+        int cnt = 0;
+        for ( int index_x=0 ; index_x<=obx; index_x++){
+            if ( index_x < otx){
+                cnt= 0 ; 
+                for ( int index_y=((oby-oty+1)%2==0 ? oty: oty+1) ; index_y<=oby; index_y+=2 ){
+                    board[index_y][index_x]=rem[cnt][index_x] ; 
+                    cnt ++ ;   
+                }
+            }
+            else {
+                cnt = 0 ; 
+                for ( int index_y=((oby-oty+1)%2==0 ? oty: oty+1); index_y<=oby; index_y+=2 ){
+                    board[index_y][index_x]= chosen[cnt][index_x-otx] ; 
+                    cnt ++ ; 
+                }
+            }
+        }
+    }
     return;
 }
 // die-type_3 reverse action 
@@ -252,9 +246,16 @@ void reverseTypeIII(int X, int Y, int size, int s)
     int chosenColNum = w / 2;
     bool first = (w + 1) % 2;
 
-    if(obx >= m){
-        if(!first) chosenColNum++;
-        first = w % 2;
+    // if((X + size - 1) >= m){
+    //     if(!first) chosenColNum++;
+    //     first = w % 2;
+    // }
+    if (0 <=X  && X < m && 0 <=Y && Y<n ){
+        if (!first) chosenColNum++; 
+        first = 1;
+    }
+    else if (abs(X)%2!=0 ){
+        first = 0 ;
     }
 
     cout << "otx:" << otx << " oty:" << oty << " obx:" << obx << " oby:" << oby << " w:" << w << " h:" << h << " chosenColNum:" << chosenColNum << " first:" << first << '\n';
@@ -262,70 +263,87 @@ void reverseTypeIII(int X, int Y, int size, int s)
     vector<int> rem[256], chosen[128];
 
     if (s==0 ) { 
-        for(int index_y = size; index_y<=oby; index_y++){
-            for(int index_x = otx; index_x<=obx; index_x+=2){                        
-                rem[index_y-(size)].push_back(board[index_y][index_x]);
+        for(int index_y = h; index_y<=oby; index_y++){
+            for(int index_x = ((obx-otx+1)%2==0 ? otx : otx+1); index_x<=obx; index_x+=2){                        
+                rem[index_y-(h)].push_back(board[index_y][index_x]);
+                cout << board[index_y][index_x]<< " " ; 
+
             }   
         }
-        cout << endl; 
-    
-        for(int index_y = 0; index_y < size; index_y++){
-            for(int index_x = otx;  index_x <= obx; index_x+=2){ 
-                chosen[index_y].push_back(board[index_y][index_x]);   
-            }                 
+        for(int index_y = 0; index_y < h; index_y++){
+            for(int index_x = ((obx-otx+1)%2==0 ? otx : otx+1);  index_x <= obx; index_x+=2){ 
+                chosen[index_y].push_back(board[index_y][index_x]); 
+                cout << board[index_y][index_x]<< " " ;  
+            }                
         }
         int cnt = 0;
-
         for ( int index_y=0 ; index_y<=oby ; index_y++){
-            if ( index_y< oty ){
+            if ( index_y< oby-h+1){
                 cnt= 0 ; 
-                for ( int index_x=otx ; index_x<=obx; index_x+=2 ){
+                for ( int index_x=((obx-otx+1)%2==0 ? otx : otx+1) ; index_x<=obx; index_x+=2 ){
                     board[index_y][index_x]=rem[index_y][cnt] ; 
                     cnt ++ ; 
-                }
-                
+                }    
             }
             else {
                 cnt = 0 ; 
-                for ( int index_x=otx ; index_x<=obx; index_x+=2 ){
-                    board[index_y][index_x]=chosen[index_y-oty][cnt] ; 
+                for ( int index_x=((obx-otx+1)%2==0 ? otx : otx+1) ; index_x<=obx; index_x+=2 ){
+                    board[index_y][index_x]=chosen[index_y-(oby-h+1)][cnt] ; 
                     cnt ++ ; 
                 }
             }
         }  
     }
     if (s==1) {
-        for(int index_y = oty; index_y < n-size; index_y++){
-            for(int index_x = otx; index_x <= obx; index_x+=2){                        
+        for(int index_y = oty; index_y < n-h; index_y++){
+            for(int index_x = ((obx-otx+1)%2==0 ? otx : otx+1); index_x <= obx; index_x+=2){                        
                 rem[index_y-(oty)].push_back(board[index_y][index_x]);
             }  
-         
+            cout << endl; 
         }
-        for(int index_y = n-size; index_y < n; index_y++){
-            for(int index_x = otx;  index_x <= obx; index_x+=2){ 
-                chosen[index_y-(n-size)].push_back(board[index_y][index_x]); 
+        cout << "bandi" << endl; 
+        for(int index_y = n-h; index_y < n; index_y++){
+            for(int index_x = ((obx-otx+1)%2==0 ? otx : otx+1);  index_x <= obx; index_x+=2){ 
+                cout << board[index_y][index_x]<< " ";
             }                 
         }
         int cnt = 0;
-        int topY=Y ; 
         for ( int index_y=oty ; index_y<n ; index_y++){
-            if ( index_y<=oby){
+            if ( index_y<oty+h){
                 cnt= 0 ; 
-                for ( int index_x=otx ; index_x<=obx; index_x+=2 ){
+                for ( int index_x=((obx-otx+1)%2==0 ? otx : otx+1) ; index_x<=obx; index_x+=2 ){
                     board[index_y][index_x]=chosen[index_y-oty][cnt] ; 
+                    cout << chosen[index_y-oty][cnt]<< "  ";
                     cnt ++ ; 
                 }
-                
             }
             else {
                 cnt = 0 ; 
-                for ( int index_x=otx ; index_x<=obx; index_x+=2 ){
-                    board[index_y][index_x]=rem[index_y-(oby+1)][cnt] ; 
+                for ( int index_x=((obx-otx+1)%2==0 ? otx : otx+1) ; index_x<=obx; index_x+=2 ){
+                    board[index_y][index_x]=rem[index_y-(oty+h)][cnt] ; 
                     cnt ++ ; 
                 }
             }
         }  
     }
+
+
+// zuv avsan butsaaj hiihdee aldsan zuvhun (X =-1 baih uyd aldaa garj bn )
+// X Y size dir 
+// -1 0 8 2 
+//  0  1  2  3  4  5 
+//  6  7  8  9 10 11 
+// 12 13 14 15 16 17 
+// 18 19 20 21 22 23 
+// 24 25 26 27 28 29 
+// 30 31 32 33 34 35 
+
+// 3  0  4  1  5  2
+//  9  6 10  7 11  8
+// 15 12 16 13 17 14
+// 21 18 22 19 23 20
+// 27 24 28 25 29 26 
+// 33 30 34 31 35 32
 
     if(s == 2){
 
@@ -375,6 +393,22 @@ void reverseTypeIII(int X, int Y, int size, int s)
             index++;
         }
     }
+// aldaa 
+// 1 0 8 3 
+//  0  1  2  3  4  5 
+//  6  7  8  9 10 11 
+// 12 13 14 15 16 17 
+// 18 19 20 21 22 23 
+// 24 25 26 27 28 29 
+// 30 31 32 33 34 35 
+
+//  2  3  0  4  1  5 
+//  8  9  6 10  7 11
+// 14 15 12 16 13 17
+// 20 21 18 22 19 23
+// 26 27 24 28 25 29 
+// 32 33 30 34 31 35
+
 
     if(s == 3){
         // CHOSEN
@@ -417,7 +451,6 @@ void reverseTypeIII(int X, int Y, int size, int s)
             }
             cnt++;
         }
-
         int index = 0;
         for(int index_x = otx + chosenColNum * 2; index_x <= obx; index_x++){
             if(obx < otx + chosenColNum * 2) break;
@@ -427,11 +460,8 @@ void reverseTypeIII(int X, int Y, int size, int s)
             index++;
         }
     }
-
     return;
 }
-
-
 void display()
 {
     for(int i = 0; i < n; i++){
@@ -444,7 +474,6 @@ void display()
     cout << "\n\n\n\n";
     return;
 }
-
 int main()
 {
     n = 6;
@@ -455,7 +484,6 @@ int main()
         }
     }
     int t = 10 ; 
-
     while ( t--){
         int size, X, Y, dir;
         cin >> X >> Y >> size>>  dir;
@@ -465,7 +493,5 @@ int main()
     // reverseTypeIII(2, 2, 4, 2);
     // display();
     }
-    
-
     return 0;
 }
