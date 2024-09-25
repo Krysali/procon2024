3e9c5b9a7d6636d9e410ff0801ea9ed180435b0c
tselmegTamir <145011490+tselmegTamir@users.noreply.github.com>
2024-09-25 21:56:51 +0800

    Update operations.py

--- TRAIN/operations.py
+++ TRAIN/operations.py
@@ -1,7 +1,7 @@
-
 import math
+import numpy as np
 
-# action(top_x, top_y, dice_num, dir)
+# action(top_x, top_y, dice_num, action.dir)
 
 def is_inside(x, y, n, m):
     if((x >= 0 and x < m) and (y >= 0 and y < n)): return 1
@@ -11,23 +11,26 @@ def apply_die(board, action):
 
     # Set up
     power = math.ceil(action.dice_num / 3)
-    size = math.pow(2, power)
-    dice_type = action.dice_num - (power * 3)
+    size = int(math.pow(2, power))
+    dice_type = action.dice_num - ((power - 1) * 3)
 
     n, m = board.shape
+
+    print(f"n:{n}, m:{m}, power:{power}, size:{size}, dice_type:{dice_type}")
 
     cut_pieces = []
 
     x_start = max(action.x, 0)
-    y_start = min(action.y, 0)
-    x_end = min(n, action.x + size) - 1
-    y_end = max(m, action.y + size) - 1
+    y_start = max(action.y, 0)
+    x_end = min(m, action.x + size) - 1
+    y_end = min(n, action.y + size) - 1
 
     width = x_end - x_start + 1
     height = y_end - y_start + 1
 
     chosenRowNum = height // 2
     chosenColNum = width // 2
+
 
     if(is_inside(action.x, action.y, n, m)):
         if (height % 2 == 1): chosenRowNum += 1
@@ -37,7 +40,10 @@ def apply_die(board, action):
             if(n % 2 == 1): chosenRowNum += 1
         if(abs(action.x) % 2 == 0):
             if(m % 2 == 1): chosenColNum += 1
-        
+
+    
+    print(f"x_start:{x_start}, x_end:{x_end}, ystart:{y_start}, y_end:{y_end}, width:{width}, height:{height}, chosenRowNum:{chosenRowNum}, chosenColNum:{chosenColNum}")
+
     # CUT PHASE
     if dice_type == 1:
         for r in range(y_start, y_end + 1):
@@ -50,19 +56,21 @@ def apply_die(board, action):
             for c in range(x_end, x_start - 1, -1):
                 cut_pieces.append(board[r][c])
                 board[r][c] = 0
-        board.reverse()
+        cut_pieces = np.flip(cut_pieces, 0)
 
     if dice_type == 3:
         for r in range(y_end, y_start - 1, -1):
             for c in range(x_end - 1, x_start - 1, -2):
                 cut_pieces.append(board[r][c])
                 board[r][c] = 0
-        board.reverse()
+        cut_pieces = np.flip(cut_pieces, 0)
 
+    print(cut_pieces)
+    print(board)
 
     # SHIFT PHASE
 
-    if dir == 0:
+    if action.dir == 0:
         for c in range(x_start, x_end + 1):
             write_index = y_start
             for r in range(y_start, n):
@@ -72,7 +80,7 @@ def apply_die(board, action):
             for r in range(write_index, n):
                 board[r][c] = 0
 
-    if dir == 1:
+    if action.dir == 1:
         for c in range(x_start, x_end + 1):
             write_index = y_end
             for r in range(y_end, -1, -1):
@@ -82,7 +90,7 @@ def apply_die(board, action):
             for r in range(write_index, -1, -1):
                 board[r][c] = 0
 
-    if dir == 2:
+    if action.dir == 2:
         for r in range(y_start, y_end + 1):
             write_index = x_start
             for c in range(x_start, m):
@@ -92,7 +100,7 @@ def apply_die(board, action):
             for c in range(write_index, m):
                 board[r][c] = 0
 
-    if dir == 3:
+    if action.dir == 3:
         for r in range(y_start, y_end + 1):
             write_index = x_end
             for c in range(x_end, -1, -1):
@@ -104,14 +112,17 @@ def apply_die(board, action):
 
 
     # BBBT PHASE
-    bxs = 0, bxe = 0, bys = 0, bye = 0
+    bxs = 0
+    bxe = 0
+    bys = 0
+    bye = 0
 
     if dice_type == 1:
-        if dir < 2:
+        if action.dir < 2:
             bxs = x_start
             bxe = x_end
-            if dir == 0:
-                bys = n - height + 1
+            if action.dir == 0:
+                bys = n - height
                 bye = n - 1
             else:
                 bys = 0
@@ -119,18 +130,18 @@ def apply_die(board, action):
         else:
             bys = y_start
             bye = y_end
-            if dir == 2:
-                bxs = m - width + 1
+            if action.dir == 2:
+                bxs = m - width
                 bxe = m - 1
             else:
                 bxs = 0
                 bye = width - 1
     if dice_type == 2:
-        if dir < 2:
+        if action.dir < 2:
             bxs = x_start
             bxe = x_end
-            if dir == 0:
-                bys = n - chosenRowNum + 1
+            if action.dir == 0:
+                bys = n - chosenRowNum
                 bye = n - 1
             else:
                 bys = 0
@@ -138,18 +149,18 @@ def apply_die(board, action):
         else:
             bys = y_start
             bye = y_end
-            if dir == 2:
-                bxs = m - width + 1
+            if action.dir == 2:
+                bxs = m - width
                 bxe = m - 1
             else:
                 bxs = 0
                 bxe = width - 1
     if dice_type == 3:
-        if dir < 2:
+        if action.dir < 2:
             bxs = x_start
             bxe = x_end
-            if dir == 0:
-                bys = n - height + 1
+            if action.dir == 0:
+                bys = n - height
                 bye = n - 1
             else:
                 bys = 0
@@ -157,8 +168,8 @@ def apply_die(board, action):
         else:
             bys = y_start
             bye = y_end
-            if dir == 2:
-                bxs = m - chosenColNum + 1
+            if action.dir == 2:
+                bxs = m - chosenColNum
                 bxe = m - 1
             else:
                 bxs = 0
@@ -168,6 +179,7 @@ def apply_die(board, action):
         cnt = 0
         for r in range(bys, bye + 1):
             for c in range(bxs, bxe + 1):
+                print(f"r:{r}, c:{c}")
                 board[r][c] = cut_pieces[cnt]
                 cnt += 1
     elif dice_type == 2:
@@ -186,5 +198,3 @@ def apply_die(board, action):
                     cnt += 1
 
     return board
-
-
