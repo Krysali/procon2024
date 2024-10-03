e66ca4f279bb4067242b24476843d315f549934f
tselmegTamir <145011490+tselmegTamir@users.noreply.github.com>
2024-10-03 15:12:17 +0800

    delete is_solved and shift_pieces

--- src/includes/ops.h
+++ src/includes/ops.h
@@ -10,12 +10,6 @@ void apply_die(GameState& game_state, Action action);
 // Function to check if a given cell is inside the board
 bool is_inside(int x, int y, int n, int m, int dtype);
 
-// Function to shift pieces on the board in a given direction
-void shift_pieces(Board& board, int direction, const std::vector<bool>& cellsToRemove);
-
-// Function to check if the puzzle is solved
-bool is_solved(const GameState& game_state);
-
 // Function to display the current game state to the console
 void display_game_state(const GameState& game_state);
 
