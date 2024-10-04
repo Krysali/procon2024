db331fc6c64b3cd79d3bc5fceea0861a0ba36afa
FuduDufu <ggdulguun3@gmail.com>
2024-10-04 16:46:37 +0800

    edits

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,16 @@
 using namespace std;
 
 int main() {
+
     std::string serverUrl = "localhost:8080";
     std::string teamToken = "token1";
     std::string problemData;
-    try {
-        problemData = GetRequest(serverUrl + "/problem", teamToken);
-    }
-    catch (runtime_error) {
-        std::cerr << "curl request failed, fallback to file json parsing\n";
-        problemData = ReadJsonFile("../problem.json");
-    }
+    problemData = ReadJsonFile("../problem.json");
     std::cout << "Problem Data:\n" << problemData << std::endl;
     GameState game_state = ParseJson(problemData);
 
     // Display the game state
-    cout << "Initial Game State:" << endl;
+    std::cout << "Initial Game State:" << endl;
     display_game_state(game_state);
 
     // example die application
@@ -27,16 +22,7 @@ int main() {
     display_game_state(game_state);
 
     // Post the answer and get the revision
-    cout << OutputJson(game_state) << endl;
-    std::string revision;
-    try {
-        revision = PostRequest(serverUrl + "/answer", teamToken, OutputJson(game_state));
-    }
-    catch (runtime_error) {
-        std::cerr << "curl request failed\n";
-        revision = "None";
-    }
-    cout << revision;
+    std::cout << OutputJson(game_state) << endl;
 
     return 0;
 }
