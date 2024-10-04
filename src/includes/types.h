b4f59a6900bc61cab4afa5b611ec1c3d07d28451
Jaydenvodkanana <javgeee@gmail.com>
2024-10-04 09:02:15 +0800

    for kenchan

--- src/includes/types.h
+++ src/includes/types.h
@@ -8,30 +8,34 @@
 const int MAX_DIMENSION = 256;
 
 // Structure to represent a die
-struct Die {
+class Die {
+public:
     int width, height;
     std::vector<std::vector<bool>> cells;  // Use bool for 0/1 values
 };
 
 // Structure to represent the game board
-struct Board {
+class Board {
+public:
     int width, height;
     std::vector<std::vector<int>> pieces;
 };
 
-struct Move {
+class Action {
+public:
     int die_index;
     int x, y;
     int direction;
 };
 
 // Structure to represent the entire game state
-struct GameState {
+class GameState {
+public:
     Board board;
     Board goal_state;
     std::vector<Die> dies;  // Includes both fixed and general dies
     int num_moves;
-    std::vector<Move> moves;
+    std::vector<Action> moves;
 };
 
 // Function to read json file
