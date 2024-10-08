4b2410727c95a4cd20069e9af2c312a97ad3ce90
LEveLiQ <leveliq6@gmail.com>
2024-10-08 11:11:47 +0800

    extreme test

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,27 @@
 #include <chrono>
 
 int main() {
-    std::string serverUrl = "localhost:8080";
-    std::string teamToken = "token1";
-    std::string problemData;
-    problemData = GetRequest(serverUrl + "/problem", teamToken);
-    // std::cout << "Problem Data:\n" << problemData << std::endl;
-    GameState game_state = ParseJson(problemData);
-    game_state.num_actions = 0;
+	std::string serverUrl = "localhost:8080";
+	std::string teamToken = "token1";
+	std::string problemData;
+	problemData = GetRequest(serverUrl + "/problem", teamToken);
+	// std::cout << "Problem Data:\n" << problemData << std::endl;
+	GameState game_state = ParseJson(problemData);
 
-    Action action = { 22, 1, 3, 2 };
-    auto start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 5000; ++i) {
-        apply_die(game_state, action);
-    }
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	display_game_state(game_state);
 
-    display_game_state(game_state);
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < 100000; i++)
+		apply_die(game_state, { i % 27, i % 200, i% 200, i % 4 });
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	
 
-    std::cout << "Optimized apply_die time: " << duration.count() << " microseconds\nAverage time:" << duration.count() / 5000 << " microseconds" << std::endl;
+	display_game_state(game_state);
 
-    PostRequest(serverUrl + "/answer", teamToken, OutputJson(game_state));
+	std::cout << "Optimized apply_die time: " << duration.count() << " microseconds\nAverage time:" << duration.count() / 100000 << " microseconds" << std::endl;
 
-    return 0;
+	PostRequest(serverUrl + "/answer", teamToken, OutputJson(game_state));
+
+	return 0;
 }
