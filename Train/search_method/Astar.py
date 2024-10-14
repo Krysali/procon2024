3baa395698ab510c0c871b3ffb9e9c6485c343a9
FuduDufu <ggdulguun3@gmail.com>
2024-10-14 11:39:45 +0800

    train.sh nemsen

--- Train/search_method/Astar.py
+++ Train/search_method/Astar.py
@@ -26,7 +26,7 @@ def main():
     parser.add_argument('--env', type=str, required=True, help="Environment: cube3, 15-puzzle, 24-puzzle")
     parser.add_argument('--batch_size', type=int, default=1, help="Batch size for BWAS")
     parser.add_argument('--weight', type=float, default=1.0, help="Weight of path cost")
-    parser.add_argument('--language', type=str, default="python", help="python or cpp")
+    parser.add_argument('--language', type=str, default="cpp", help="python or cpp")
 
     parser.add_argument('--results_dir', type=str, required=True, help="Directory to save results")
     parser.add_argument('--start_idx', type=int, default=0, help="")
