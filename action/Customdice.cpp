483299c53dc827d8a1a194716ffd5ddca5b507d9
Zenithi77 <tamiraatami777@gmail.com>
2024-10-14 21:46:07 +0800

    actual finale version

--- action/Customdice.cpp
+++ action/Customdice.cpp
@@ -23,121 +23,141 @@ void output(int n , int m , vector<vector<int>> a) {
     }
 }
 
-// vector<vector<int>> bigger2(int n , int m , vector<vector<int>> a , int level) {
-//     output(n , m , a);
-//     if(pow(2 , level - 1) <= n + 1 && pow(2 , level - 1) <= m + 1) {
-//         for(int i = 0; i < n - level + 2; i++) {
-//             for(int l = 0; l < m - level + 2; l++) {
-//                 if(a[i][l] >= level * -1 && a[i][l] <= level * -1 + 1 && a[i + pow(2 , level - 2)][l] >= level * -1 && a[i + pow(2 , level - 2)][l] <= level * -1 + 1 && a[i][l + pow(2 , level - 2)] >= level * -1 && a[i][l + pow(2 , level - 2)] <= level * -1 + 1 && a[i + pow(2 , level - 2)][l + pow(2 , level - 2)] >= level * -1 && a[i + pow(2 , level - 2)][l + pow(2 , level - 2)] <= level * -1 + 1) {
-//                     a[i][l] = level * -1;
-//                 }
-//             }
-//         }
-//         a = bigger2(n , m , a , level + 1);
-//         if(level > 2) {
-//             for(int i = 0; i < n - level + 1; i++) {
-//                 for(int l = 0; l < m - level + 1; l++) {
-//                     if(a[i][l] == (level - 1) * -1) {
-//                         if(a[i + pow(2 , level - 3)][l] != 0 || a[i][l + pow(2 , level - 3)] != 0  || a[i + pow(2 , level - 3)][l + pow(2 , level - 3)] != 0) {
-//                             a[i][l]++;
-//                         } else {
-//                             gaia variant(i , l , (level - 2) * 3 - 2);
-//                             phantom.push_back(variant);
-//                             for(int ii = i; ii < i + pow(2 , level - 3) - 1; ii++) {
-//                                 for(int ll = l; ll < l + pow(2 , level - 3) - 1; ll++) {
-//                                     a[ii][ll] = 0;
-//                                 }
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         } else {
-//             for(int i = 0; i < n; i++) {
-//                 for(int l = 0; l < m; l++) {
-//                     if(a[i][l] == -1) {
-//                         gaia variant(l , i , 0);
-//                         phantom.push_back(variant);
-//                         a[i][l] = 0;
-//                     }
-//                 }
-//             }
-//         }
-//         return a;
-//     } else {
-//         for(int i = 0; i < n; i++) {
-//             for(int l = 0; l <m; l++) {
-//                 if(a[i][l] == -1 * (level - 1) ) {
-//                     int ares = i , odin = l;
-//                     gaia variant(odin , ares , (level - 2) * 3 - 2);
-//                     phantom.push_back(variant);
-//                     for(int ii = ares; ii <= ares + pow(2 , level - 2) - 1; ii++) {
-//                         for(int ll = odin; ll <= odin + pow(2 , level - 2) - 1; ll++) {
-//                             a[ii][ll] = 0;
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         return a;
-//     }
-// }
+vector<vector<int>> bigger2(int n , int m , vector<vector<int>> a , int level) {
+    if(pow(2 , level) <= n + 1 && pow(2 , level) <= m + 1) {
+        for(int i = 0; i <= n - pow(2 , level) + 1; i++) {
+            for(int l = 0; l <= m - pow(2 , level); l++) {
+                if(a[i][l] == level && a[i][l + pow(2 , level - 1)] == level) {
+                    a[i][l] = level + 1;
+                }
+            }
+        }
+        cout << level << endl;
+        output(n , m , a);
+        a = bigger2(n , m , a , level + 1);
+        if(level > 1) {
+            for(int i = 0; i <= n - pow(2 , level - 1) + 1; i++) {
+                for(int l = 0; l <= m - pow(2 , level - 1); l++) {
+                    if(a[i][l] == level) {
+                        if(a[i][l + pow(2 , level - 2)] == 0) {
+                            a[i][l]--;
+                        } else {
+                            gaia variant(l , i , (level - 1) * 3 - 1);
+                            phantom.push_back(variant);
+                            for(int ii = i; ii < i + pow(2 , level - 1); ii += 2) {
+                                for(int ll = l; ll < l + pow(2 , level - 1); ll++) {
+                                    a[ii][ll] = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            cout << level << endl;
+            output(n , m , a);
+        } else {
+            for(int i = 0; i < n; i++) {
+                for(int l = 0; l < m; l++) {
+                    if(a[i][l] == 1) {
+                        gaia variant(l , i , 0);
+                        phantom.push_back(variant);
+                        a[i][l] = 0;
+                    }
+                }
+            }
+            cout << level << endl;
+            output(n , m , a);
+        }
+        return a;
+    } else {
+        for(int i = 0; i < n; i++) {
+            for(int l = 0; l < m ; l++) {
+                if(a[i][l] == level) {
+                    int ares = i , odin = l;
+                    gaia variant(odin , ares , (level - 1) * 3 - 1);
+                    phantom.push_back(variant);
+                    for(int ii = ares; ii < ares + pow(2 , level - 1); ii += 2) {
+                        for(int ll = odin; ll < odin + pow(2 , level - 1); ll++) {
+                            a[ii][ll] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        cout << level << endl;
+        output(n , m , a);
+        return a;
+    }
+}
 
-// vector<vector<int>> bigger3(int n , int m , vector<vector<int>> a , int level) {
-//     if(pow(2 , level - 1) <= n + 1 && pow(2 , level - 1) <= m + 1) {
-//         for(int i = 0; i < n - level + 2; i++) {
-//             for(int l = 0; l < m - level + 2; l++) {
-//                 if(a[i][l] >= level * -1 && a[i][l] <= level * -1 + 1 && a[i + pow(2 , level - 2)][l] >= level * -1 && a[i + pow(2 , level - 2)][l] <= level * -1 + 1 && a[i][l + pow(2 , level - 2)] >= level * -1 && a[i][l + pow(2 , level - 2)] <= level * -1 + 1 && a[i + pow(2 , level - 2)][l + pow(2 , level - 2)] >= level * -1 && a[i + pow(2 , level - 2)][l + pow(2 , level - 2)] <= level * -1 + 1) {
-//                     a[i][l] = level * -1;
-//                 }
-//             }
-//         }
-//         a = bigger3(n , m , a , level + 1);
-//         if(level > 2) {
-//             for(int i = 0; i < n - level + 1; i++) {
-//                 for(int l = 0; l < m - level + 1; l++) {
-//                     if(a[i][l] == (level - 1) * -1) {
-//                         if(a[i + pow(2 , level - 3)][l] != 0 || a[i][l + pow(2 , level - 3)] != 0  || a[i + pow(2 , level - 3)][l + pow(2 , level - 3)] != 0) {
-//                             a[i][l]++;
-//                         } else {
-//                             gaia variant(i , l , (level - 2) * 3 - 2);
-//                             phantom.push_back(variant);
-//                             for(int ii = i; ii < i + pow(2 , level - 3) - 1; ii++) {
-//                                 for(int ll = l; ll < l + pow(2 , level - 3) - 1; ll++) {
-//                                     a[ii][ll] = 0;
-//                                 }
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         } else {
-//             for(int i = 0; i < n; i++) {
-//                 for(int l = 0; l < m; l++) {
-//                     if(a[i][l] == -1) {
-//                     }
-//                 }
-//             }
-//         }
-//         return a;
-//     } else {
-//         for(int i = 0; i < n; i++) {
-//             for(int l = 0; l <m; l++) {
-//                 if(a[i][l] == -1 * (level - 1) ) {
-//                     int ares = i , odin = l;
-//                     gaia variant(odin , ares , (level - 2) * 3 - 2);
-//                     phantom.push_back(variant);
-//                     for(int ii = ares; ii <= ares + pow(2 , level - 2) - 1; ii++) {
-//                         for(int ll = odin; ll <= odin + pow(2 , level - 2) - 1; ll++) {
-//                             a[ii][ll] = 0;
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//         return a;
-//     }
-// }
+vector<vector<int>> bigger3(int n , int m , vector<vector<int>> a , int level) {
+    if(pow(2 , level) <= n && pow(2 , level) <= m + 1) {
+        for(int i = 0; i <= n - pow(2 , level); i++) {
+            for(int l = 0; l <= m - pow(2 , level) + 1; l++) {
+                if(a[i][l] == level && a[i + pow(2 , level - 1)][l] == level) {
+                    a[i][l] = level + 1;
+                }
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+        cout << level << endl;
+        output(n , m , a);
+        a = bigger3(n , m , a , level + 1);
+        if(level > 1) {
+            for(int i = 0; i <= n - pow(2 , level - 1); i++) {
+                for(int l = 0; l <= m - pow(2 , level - 1) + 1; l++) {
+                    if(a[i][l] == level) {
+                        if(a[i + pow(2 , level - 2)][l] == 0) {
+                            a[i][l]--;
+                        } else {
+                            gaia variant(l , i , (level - 1) * 3);
+                            phantom.push_back(variant);
+                            for(int ii = i; ii < i + pow(2 , level - 1); ii++) {
+                                for(int ll = l; ll < l + pow(2 , level - 1); ll += 2) {
+                                    a[ii][ll] = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            cout << level << endl;
+            output(n , m , a);
+        } else {
+            for(int i = 0; i < n; i++) {
+                for(int l = 0; l < m; l++) {
+                    if(a[i][l] == 1) {
+                        gaia variant(l , i , 0);
+                        phantom.push_back(variant);
+                        a[i][l] = 0;
+                    }
+                }
+            }
+            cout << level << endl;
+            output(n , m , a);
+        }
+        return a;
+    } else {
+        for(int i = 0; i < n; i++) {
+            for(int l = 0; l < m ; l++) {
+                if(a[i][l] == level) {
+                    int ares = i , odin = l;
+                    gaia variant(odin , ares , (level - 1) * 3);
+                    phantom.push_back(variant);
+                    for(int ii = ares; ii < ares + pow(2 , level - 1); ii++) {
+                        for(int ll = odin; ll < odin + pow(2 , level - 1); ll += 2) {
+                            a[ii][ll] = 0;
+                        }
+                    }
+                }
+            }
+        }
+        cout << level << endl;
+        output(n , m , a);
+        return a;
+    }
+}
 
 vector<vector<int>> bigger(int n , int m , vector<vector<int>> a , int level , int direction) {
     if(pow(2 , level) <= n && pow(2 , level) <= m) {
@@ -148,12 +168,10 @@ vector<vector<int>> bigger(int n , int m , vector<vector<int>> a , int level , i
                 }
             }
         }
-        cout << level << endl;
-        output(n ,m ,a);
         a = bigger(n , m , a , level + 1 , direction);
         if(level > 1) {
-            for(int i = 0; i < n - pow(2 , level - 1); i++) {
-                for(int l = 0; l < m - pow(2 , level - 1); l++) {
+            for(int i = 0; i <= n - pow(2 , level - 1); i++) {
+                for(int l = 0; l <= m - pow(2 , level - 1); l++) {
                     if(a[i][l] == level) {
                         if(a[i + pow(2 , level - 2)][l] != level - 1 || a[i][l + pow(2 , level - 2)] != level - 1 || a[i + pow(2 , level - 2)][l + pow(2 , level - 2)] != level - 1) {
                             a[i][l]--;
@@ -169,13 +187,13 @@ vector<vector<int>> bigger(int n , int m , vector<vector<int>> a , int level , i
                     }
                 }
             }
-            cout << level << endl;
-            output(n ,m ,a);
         } else {
             if(direction == 0 || direction == 1) {
-                //a = bigger3(n , m , a , 2);
+                output(n , m , a);
+                a = bigger3(n , m , a , 1);
             } else {
-                //a = bigger2(n , m , a , 2);
+                output(n , m , a);
+                a = bigger2(n , m , a , 1);
             }
         }
         return a;
@@ -186,7 +204,7 @@ vector<vector<int>> bigger(int n , int m , vector<vector<int>> a , int level , i
                     int ares = i , odin = l;
                     gaia variant(odin , ares , (level - 1) * 3 - 2);
                     phantom.push_back(variant);
-                    cout << ares << " " << odin << endl;
+                    //cout << ares << " " << odin << endl;
                     for(int ii = ares; ii < ares + pow(2 , level - 1); ii++) {
                         for(int ll = odin; ll < odin + pow(2 , level - 1); ll++) {
                             a[ii][ll] = 0;
@@ -195,15 +213,12 @@ vector<vector<int>> bigger(int n , int m , vector<vector<int>> a , int level , i
                 }
             }
         }
-        cout << level << endl;
-        output(n ,m ,a);
         return a;
     }
 }
 
 void ghost(int n , int m , vector<vector<int>> a , int direction) {
     a = bigger(n , m , a , 1 , direction);
-    output(n, m , a);
     for(auto fk : phantom) {
         cout << fk.x << " " << fk.y << " " << fk.dice_num << endl;
     }
