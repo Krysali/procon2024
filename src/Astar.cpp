cf83aca08ae2fdde6e571eb7add85e93b891a10a
tselmegTamir <tslmgtamir@gmail.com>
2024-10-16 12:01:22 +0800

    Astar

--- src/Astar.cpp
+++ src/Astar.cpp
@@ -3,7 +3,7 @@
 #include "torch/torch.h"
 
 int main() {
-    torch::jit::script::Module net = torch::jit::load("../models/pro_model.pt");
+    torch::jit::script::Module model = torch::jit::load("../models/pro_model.pt");
     torch::Tensor x = torch::randn({1, 8, 256, 256});
     std::cout << x;
     return 0;
